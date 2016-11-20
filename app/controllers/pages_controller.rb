1ed260ae1ea26e1e26cca3c711f49bdca6e516cd
s-akama <shota_akama@MacBook-Pro.local>
2016-11-20 17:31:15 +0900

    インデント修正 html.erb 不要タグ削除

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,32 +2,30 @@ class PagesController < ApplicationController
   def page1
   end
 
-   def visit
-   end
-
-   def others
-      Slack.chat_postMessage text: "------------------------------\n\n\n\n\n \"Others\" is coming \n\n\n\n\n ------------------------------", username: "Kitayo_Notifier", channel: "#kitayo"
-      @hide_flg = true
-   end
-
-   def inter
-     Slack.chat_postMessage text: "------------------------------\n\n\n\n\n \"Interviewer\" is coming \n\n\n\n\n ------------------------------", username: "Kitayo_Notifier", channel: "#kitayo"
-     @hide_flg = true
-   end
-
-   def appointment
-   end
-
-   def noAppointment
-     Slack.chat_postMessage text: "------------------------------\n\n\n\n\n \"Non-appointment\" is coming \n\n\n\n\n ------------------------------", username: "Kitayo_Notifier", channel: "#kitayo"
-     @hide_flg = true
-   end
-
-   def Call
-     @hide_flg = true
-     @name = params[:n]
-    #Slack.chat_postMessage text: "------------------------------
-    #\n\n\n\n\n @#{@name} \n \"appointment\" is coming \n\n\n\n\n ------------------------------", username: "Kitayo_Notifier", channel: "#kitayo"
-   end
-
+  def visit
   end
+
+  def others
+    Slack.chat_postMessage text: "------------------------------\n\n\n\n\n \"Others\" is coming \n\n\n\n\n ------------------------------", username: "Kitayo_Notifier", channel: "#kitayo"
+    @hide_flg = true
+  end
+
+  def inter
+   Slack.chat_postMessage text: "------------------------------\n\n\n\n\n \"Interviewer\" is coming \n\n\n\n\n ------------------------------", username: "Kitayo_Notifier", channel: "#kitayo"
+   @hide_flg = true
+  end
+
+  def appointment
+  end
+
+  def noAppointment
+   Slack.chat_postMessage text: "------------------------------\n\n\n\n\n \"Non-appointment\" is coming \n\n\n\n\n ------------------------------", username: "Kitayo_Notifier", channel: "#kitayo"
+   @hide_flg = true
+  end
+
+  def call
+   @hide_flg = true
+   @name = params[:n]
+   Slack.chat_postMessage text: "------------------------------\n\n\n\n\n @#{@name} \n \"appointment\" is coming \n\n\n\n\n ------------------------------", username: "Kitayo_Notifier", channel: "#kitayo"
+  end
+end
