66279ff1b99b56d1f895bf18ffb86bc4ffed17ae
jingo4it0923 <j.tanaka@10baton.com>
2016-11-23 21:40:02 +0900

    CSS

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -43,9 +43,9 @@ class PagesController < ApplicationController
   def call
     @name = params[:n]
     # Slack.chat_postMessage text: "------------------------------\n\n\n\n\n @#{@name} \n \"appointment\" is coming \n\n\n\n\n ------------------------------", username: "Kitayo_Notifier", channel: "#kitayo"
-    notifier = Slack::Notifier.new('https://hooks.slack.com/services/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')
-    @massage = "------------------------------\n\n\n\n\n @#{@name} \n \"appointment\" is coming \n\n\n\n\n ------------------------------"
-    notifier.ping @massage
+    #notifier = Slack::Notifier.new('https://hooks.slack.com/services/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')
+    #@massage = "------------------------------\n\n\n\n\n @#{@name} \n \"appointment\" is coming \n\n\n\n\n ------------------------------"
+    #notifier.ping @massage
     @hide_flg = true
   end
 end
