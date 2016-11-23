b32a9f38799ff2ab5ee12d47928ece1f0a30c09d
Shota Akama <s.akama@10baton.com>
2016-11-23 12:48:36 +0900

    x

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,7 @@ class PagesController < ApplicationController
     @a = {
       color: "good"
     }
-    notifier.ping(@massage), attachments: [a]
+    notifier.ping @massage, attachments: [a]
     @hide_flg = true
   end
 
