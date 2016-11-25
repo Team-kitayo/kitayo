bd7de16281c2b450c44866e93915a9cc0e6d5c32
Shota Akama <s.akama@10baton.com>
2016-11-25 21:17:10 +0900

    slack massage更新

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def notifier
+    $name
+    @property
+    @color
+    @current = Time.now.to_i
+    @massage = {
+      "attachments": [
+        {
+        "title": "@#{$name}",
+        "text": "\"#{@property}\" is coming",
+        "color": "##{@color}",
+        "ts": @current
+        }
+      ]
+    }
+    notifier = Slack::Notifier.new('https://hooks.slack.com/services/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')
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,50 +1,39 @@
 class PagesController < ApplicationController
   def page1
+    $name = "Channel"
   end
 
   def visit
   end
 
   def others
-    # Slack.chat_postMessage text: "------------------------------\n\n\n\n\n \"Others\" is coming \n\n\n\n\n ------------------------------", username: "Kitayo_Notifier", channel: "#kitayo"
-    notifier = Slack::Notifier.new('https://hooks.slack.com/services/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')
-    @massage = "------------------------------\n\n\n\n\n \"Others\" is coming \n\n\n\n\n ------------------------------"
+    @property = "Others"
+    @color = "FFD742"
     notifier.ping @massage
     @hide_flg = true
   end
 
   def inter
-  #  Slack.chat_postMessage text: "------------------------------\n\n\n\n\n \"Interviewer\" is coming \n\n\n\n\n ------------------------------", username: "Kitayo_Notifier", channel: "#kitayo"
-   notifier = Slack::Notifier.new('https://hooks.slack.com/services/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')
-   @massage = "-----------------------------\n\n\n\n\n \"Interviewer\" is coming \n\n\n\n\n ------------------------------"
-   notifier.ping @massage
-   @hide_flg = true
+    @property = "Interviewer"
+    @color = "FF2020"
+    notifier.ping @massage
+    @hide_flg = true
   end
 
   def appointment
   end
 
   def noAppointment
-  #  Slack.chat_postMessage text: "------------------------------\n\n\n\n\n \"Non-appointment\" is coming \n\n\n\n\n ------------------------------", username: "Kitayo_Notifier", channel: "#kitayo"
-    notifier = Slack::Notifier.new('https://hooks.slack.com/services/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')
-    # @massage = "------------------------------\n\n\n\n\n \"Non-appointment\" is coming \n\n\n\n\n ------------------------------"
-    @massage = {
-      "attachments": [
-        {
-        "text": "\"Non-appointment\" is coming",
-			  "color": "#3E9DEF"
-        }
-      ]
-    }
+    @property = "Non-appointment"
+    @color = "06A4E4"
     notifier.ping @massage
     @hide_flg = true
   end
 
   def call
-    @name = params[:n]
-     Slack.chat_postMessage text: "------------------------------\n\n\n\n\n @#{@name} \n \"appointment\" is coming \n\n\n\n\n ------------------------------", username: "Kitayo_Notifier", channel: "#kitayo"
-    notifier = Slack::Notifier.new('https://hooks.slack.com/services/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')
-    @massage = "------------------------------\n\n\n\n\n @#{@name} \n \"appointment\" is coming \n\n\n\n\n ------------------------------"
+    $name = "#{params[:n]}"
+    @property = "Appointment"
+    @color = "06A4E4"
     notifier.ping @massage
     @hide_flg = true
   end
